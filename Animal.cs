2d879524902f5dec64970f1acef0c06adfebfd9a
yta repp <reppyta4@gmail.com>
2022-09-14 21:00:01 -0300

    chau

--- Animal.cs
+++ Animal.cs
@@ -9,6 +9,7 @@ namespace TP_I_Lab
     abstract class Animal : IComparable
     {
         //hola
+        //chau
         public int PasosParaMorir { get; private set; }
         public int TiempoSinComer { get; private set; }
         public int PasosVividos { get; private set; }
